043142f3fa0026fd326079e23c0060b93d32ffa4
Danimexivasco <dcano9493@gmail.com>
2025-04-06 14:12:54 +0200

    Add Caesar font

---
+++ assets/fonts/index.ts
@@ -0,0 +1,8 @@
+import { Caesar_Dressing } from "next/font/google";
+
+export const caesarDressing = Caesar_Dressing({
+  weight:   "400",
+  variable: "--font-caesar-dressing",
+  subsets:  ["latin"],
+  display:  "swap"
+});
