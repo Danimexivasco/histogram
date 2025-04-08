89343d4d09411ac4e45821aedc09d5dbc7c5327b
Danimexivasco <dcano9493@gmail.com>
2025-04-08 08:15:54 +0200

    Create initial routes

---
+++ lib/routes.ts
@@ -0,0 +1,8 @@
+export const routes = {
+  home:         "/",
+  login:        "/login",
+  register:     "/register",
+  confirmEmail: "/confirm-email"
+};
+
+export const routesWithoutHeader = [routes.login, routes.register];
