2c45ea160d5ae55c2aa11b8a5fdddcebcd4bc235
Danimexivasco <dcano9493@gmail.com>
2025-04-13 12:55:33 +0200

    Refactor authStateChanged hook

--- hooks/useAuthStateChanged.ts
+++ hooks/useAuthStateChanged.ts
@@ -4,6 +4,32 @@ import { supabase } from "@/lib/supabase/client";
 import { ProfileService } from "@/services/Profile";
 import { ToastService } from "@/services/Toast";
 import { routes } from "@/lib/routes";
+import { User } from "@supabase/supabase-js";
+
+const handleGoogleLoginProfileCreation = async (user: User) => {
+  const profileExist = await ProfileService.checkIfProfileExists({
+    user_id: user.id
+  });
+
+  if (!profileExist) {
+    try {
+      const { id, email, user_metadata } = user;
+      const { full_name, avatar_url, picture } = user_metadata;
+
+      await ProfileService.create({
+        id:         id,
+        fullname:   full_name ?? "",
+        username:   full_name ?? "",
+        avatar_url: avatar_url ?? picture,
+        email:      email
+      });
+
+      ToastService.success("User created successfully");
+    } catch {
+      ToastService.error("Error creating the profile for Google");
+    }
+  }
+};
 
 export const useAuthStateChange = () => {
   const router = useRouter();
@@ -14,31 +40,8 @@ export const useAuthStateChange = () => {
         if (event === "SIGNED_IN" && session?.user) {
           const user = session.user;
 
-          // Handle Google OAuth login
           if (user?.app_metadata.provider === "google") {
-            const profileExist = await ProfileService.checkIfProfileExists({
-              user_id: user.id
-            });
-
-            if (!profileExist) {
-              try {
-                const { id, email, user_metadata } = user;
-                const { full_name, avatar_url, picture } = user_metadata;
-
-                await ProfileService.create({
-                  id:         id,
-                  fullname:   full_name ?? "",
-                  username:   full_name ?? "",
-                  avatar_url: avatar_url ?? picture,
-                  email:      email
-                });
-
-                ToastService.success("User created successfully");
-              } catch (error) {
-                ToastService.error("Error creating the profile for Google");
-                console.error("Error creating profile:", error);
-              }
-            }
+            handleGoogleLoginProfileCreation(user);
           }
 
           router.push(routes.home);
