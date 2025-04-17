7c7e5c36abd4974c66b7e4d43b39df61b1630ae4
Danimexivasco <dcano9493@gmail.com>
2025-04-17 12:57:01 +0200

    Add username strict rules on schemas

--- schema/authForm.ts
+++ schema/authForm.ts
@@ -1,7 +1,9 @@
 import z from "zod";
 
 export const authFormSchema = z.object({
-  username: z.string().min(3, "Username must contain at least 3 characters"),
+  username: z.string() .min(3, "Username must contain at least 3 characters")
+    .max(30, "Username must not exceed 30 characters")
+    .regex(/^[a-z0-9_]+$/, "Username can only contain lowercase letters, numbers, and underscores"),
   email:    z.string().email(),
   password: z.string().min(6, "Password must contain at least 6 characters")
 });
--- schema/profile.ts
+++ schema/profile.ts
@@ -1,12 +1,15 @@
+import { BIO_MAX_LENGTH } from "@/lib/constants";
 import z from "zod";
 
 export const profileSchema = z.object({
-  id:         z.string(),
-  user_id:    z.string(),
-  email:      z.string().email(),
-  username:   z.string().min(3, "Username must contain at least 3 characters"),
+  id:       z.string(),
+  user_id:  z.string(),
+  email:    z.string().email(),
+  username: z.string() .min(3, "Username must contain at least 3 characters")
+    .max(30, "Username must not exceed 30 characters")
+    .regex(/^[a-z0-9_]+$/, "Username can only contain lowercase letters, numbers, and underscores"),
   fullname:   z.string().optional(),
-  bio:        z.string().optional().nullable(),
+  bio:        z.string().max(BIO_MAX_LENGTH, "Username must not exceed 30 characters").optional().nullable(),
   avatar_url: z.string().url().optional().nullable(),
   created_at: z.string(),
   updated_at: z.string()
