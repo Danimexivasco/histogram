5dd8af6f96ae23949fd7b615462efe20ac18b60e
Danimexivasco <dcano9493@gmail.com>
2025-04-17 12:44:00 +0200

    Create asideMenu and bottomtabBar components

---
+++ components/AsideMenu.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { routes } from "@/lib/routes";
+import Link from "./ui/Link";
+import {
+  BookOpenIcon,
+  HeartIcon,
+  HomeIcon,
+  PlusIcon,
+  SearchIcon
+} from "lucide-react";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+import Avatar from "./Avatar";
+import { useAuthUser } from "@/hooks/useAuth";
+import { useGetProfile } from "@/hooks/useProfile";
+import useHideLayoutElements from "@/hooks/useHideLayoutElements";
+
+// TODO: Change links when creating the pages
+export default function AsideMenu() {
+  const pathname = usePathname();
+  const { data: user } = useAuthUser();
+  const { data: profile } = useGetProfile(user?.id ?? "");
+  const withoutAsideMenu = useHideLayoutElements();
+
+  if (withoutAsideMenu) return null;
+
+  return (
+    <aside className="hidden md:block sticky top-20 p-4 space-y-8 self-start">
+      <Link
+        href={routes.home}
+        className="grid place-items-center no-underline space-y-0.5"
+      >
+        <HomeIcon
+          size={26}
+          className={cn(pathname === routes.home && "text-spartan-500")}
+        />
+        <small>Home</small>
+      </Link>
+      <Link
+        href={routes.home}
+        className="grid place-items-center no-underline space-y-0.5"
+      >
+        <SearchIcon size={26} />
+        <small>Search</small>
+      </Link>
+      <Link
+        href={routes.home}
+        className="grid place-items-center no-underline space-y-0.5"
+      >
+        <HeartIcon size={26} />
+        <small>Notifications</small>
+      </Link>
+      <Link
+        href={routes.home}
+        className="grid place-items-center no-underline space-y-0.5"
+      >
+        <BookOpenIcon size={26} />
+        <small>Library</small>
+      </Link>
+      {user &&
+      <div className="space-y-8">
+        <Link
+          href={routes.home}
+          className="grid place-items-center no-underline space-y-0.5"
+        >
+          <div className="bg-spartan-500 p-3 rounded-full shadow text-white">
+            <PlusIcon
+              size={30}
+            />
+          </div>
+          <small>Add post</small>
+        </Link>
+        <Link
+          href={routes.profile.replace(":username", profile?.username ?? "")}
+          className="grid place-items-center no-underline space-y-0.5"
+        >
+          <Avatar
+            src={profile?.avatar_url ?? ""}
+            fallback={profile?.username?.[0].toUpperCase() ?? profile?.fullname?.[0].toUpperCase() ?? ""}
+          />
+          <small>Profile</small>
+        </Link>
+      </div>
+      }
+    </aside>
+  );
+}
---
+++ components/BottomTabBar.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useAuthUser } from "@/hooks/useAuth";
+import { useGetProfile } from "@/hooks/useProfile";
+import {
+  BookOpenIcon,
+  HomeIcon,
+  LogInIcon,
+  PlusIcon,
+  SearchIcon
+} from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/Avatar";
+import Link from "./ui/Link";
+import { routes } from "@/lib/routes";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Skeleton } from "./ui/Skeleton";
+import useHideLayoutElements from "@/hooks/useHideLayoutElements";
+
+// TODO: Change links when creating the pages
+export default function BottomTabBar() {
+  const pathname = usePathname();
+  const { data: user, isFetching: fetchingUser } = useAuthUser();
+  const { data: profile } = useGetProfile(user?.id ?? "");
+  const withoutBottomTabBar = useHideLayoutElements();
+
+  const noUser = !user && !fetchingUser;
+  const withUser = user && !fetchingUser;
+
+  if (withoutBottomTabBar) return null;
+
+  return (
+    <section className="flex items-center justify-around md:hidden bg-background w-full h-14 fixed bottom-0 border-t-1 border-t-accent-foreground shadow-[0px_-20px_20px_0px_rgba(0,0,15,0.1)]">
+      <Link href={routes.home}>
+        <HomeIcon
+          size={26}
+          className={cn(pathname === routes.home && "text-spartan-500")}
+        />
+      </Link>
+      <SearchIcon size={26} />
+      <div className="bg-spartan-500 p-3 rounded-full -translate-y-4 shadow text-white">
+        <PlusIcon
+          size={30}
+        />
+      </div>
+      <BookOpenIcon size={26} />
+      {fetchingUser && <Skeleton className="w-8 h-8 rounded-full" />}
+      {noUser &&
+      <Link href={routes.login}>
+        <LogInIcon size={26}/>
+      </Link>
+      }
+      {withUser && (
+        <Link
+          href={routes.profile.replace(":username", profile?.username ?? "")}
+          className={cn("no-underline rounded-full", pathname === `/${profile?.username}` && "ring-2 ring-spartan-500")}
+        >
+          <Avatar className="w-8 h-8">
+            <AvatarImage src={profile?.avatar_url ?? ""}/>
+            <AvatarFallback>{profile?.username?.[0].toUpperCase() ?? profile?.fullname?.[0].toUpperCase()}</AvatarFallback>
+          </Avatar>
+        </Link>
+      )}
+    </section>
+  );
+}
