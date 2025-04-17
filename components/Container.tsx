a66d896b5e9d82dfe141f06820eb7d502518ae92
Danimexivasco <dcano9493@gmail.com>
2025-04-17 12:54:14 +0200

    Add centered prop to container

--- components/Container.tsx
+++ components/Container.tsx
@@ -2,12 +2,13 @@ import clsx from "clsx";
 
 type ContainerProps = {
   children: React.ReactNode;
+  centered?: boolean;
   className?: string;
 };
 
-export default function Container({ children, className }: ContainerProps) {
+export default function Container({ children, centered, className }: ContainerProps) {
   return (
-    <div className={clsx("@container mx-auto px-4 md:px-8", className)}>
+    <div className={clsx("@container mx-auto px-4 md:px-8", centered && "max-w-7xl mx-auto", className)}>
       {children}
     </div>
   );
