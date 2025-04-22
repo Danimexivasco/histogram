5b1e1f233655bd730966f8c39eb8c95c83d94dad
Danimexivasco <dcano9493@gmail.com>
2025-04-22 14:03:18 +0200

    Handle carousel navigation visibility

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,6 +1,6 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
-import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { Swiper, SwiperRef, SwiperSlide, useSwiper } from "swiper/react";
 import { Pagination, Navigation } from "swiper/modules";
 import { Post } from "@/schema/post";
 import { Button } from "./ui/Button";
@@ -9,10 +9,14 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-function SlidePrevButton() {
+type NavigationButtonProps = {
+  isVisible: boolean;
+};
+
+function SlidePrevButton({ isVisible }: NavigationButtonProps) {
   const swiper = useSwiper();
 
-  return (
+  return isVisible && (
     <Button
       className="absolute top-1/2 -translate-y-1/2 left-2 md:left-4 w-6 h-6 md:w-10 md:h-10 rounded-full z-50 opacity-85"
       variant={"spartan"}
@@ -23,10 +27,10 @@ function SlidePrevButton() {
   );
 }
 
-function SlideNextButton() {
+function SlideNextButton({ isVisible }: NavigationButtonProps) {
   const swiper = useSwiper();
 
-  return (
+  return isVisible && (
     <Button
       className="absolute top-1/2 -translate-y-1/2 right-2 md:right-4 w-6 h-6 md:w-10 md:h-10 rounded-full z-50 opacity-85"
       variant={"spartan"}
@@ -38,7 +42,28 @@ function SlideNextButton() {
 }
 
 export default function Carousel({ mediaItems }: {mediaItems: Post["media"]}) {
-  const swiperRef = useRef(null);
+  const swiperRef = useRef<SwiperRef | null>(null);
+  const [isPrevVisible, setIsPrevVisible] = useState(true);
+  const [isNextVisible, setIsNextVisible] = useState(true);
+
+  const handleSlideChange = () => {
+    const swiper = swiperRef.current?.swiper;
+    const isFirstSlide = swiper?.activeIndex === 0;
+    const isLastSlide = swiper?.slides && swiper.slides.length - 1 === swiper.activeIndex;
+
+    setIsPrevVisible(!isFirstSlide);
+    setIsNextVisible(!isLastSlide);
+  };
+
+  useEffect(() => {
+    const swiper = swiperRef.current?.swiper;
+    swiper?.on("slideChange", handleSlideChange);
+    handleSlideChange();
+
+    return () => {
+      swiper?.off("slideChange", handleSlideChange);
+    };
+  }, []);
 
   return mediaItems && (
     <Swiper
@@ -66,8 +91,8 @@ export default function Carousel({ mediaItems }: {mediaItems: Post["media"]}) {
           <Media media={media} />
         </SwiperSlide>
       ))}
-      <SlidePrevButton />
-      <SlideNextButton />
+      <SlidePrevButton isVisible={isPrevVisible} />
+      <SlideNextButton isVisible={isNextVisible} />
     </Swiper>
   );
 }
