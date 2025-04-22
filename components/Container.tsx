7c8a7e9d8d097a59d6efa42915b9de22afdb1b52
Danimexivasco <dcano9493@gmail.com>
2025-04-22 14:05:01 +0200

    Adjust container

--- components/Container.tsx
+++ components/Container.tsx
@@ -9,7 +9,7 @@ type ContainerProps = {
 
 export default function Container({ children, centered, fullWidth, className }: ContainerProps) {
   return (
-    <div className={cn("@container w-full mx-auto px-4 md:px-8 max-w-2xl", centered && "mx-auto", fullWidth && "max-w-full", className)}>
+    <div className={cn("@container w-full mx-auto px-4 md:px-8 max-w-3xl", centered && "mx-auto", fullWidth && "max-w-full", className)}>
       {children}
     </div>
   );
